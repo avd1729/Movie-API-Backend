c919f5f2f04214e0b32db1514cada716111335b8
avd <aravind19112003@gmail.com>
2023-11-28 15:11:12 +0530

    Search by Movie ID

--- src/main/java/dev/aravind/movies/MovieService.java
+++ src/main/java/dev/aravind/movies/MovieService.java
@@ -1,9 +1,12 @@
 package dev.aravind.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 // Business Logic will go here
 @Service
 public class MovieService {
@@ -11,5 +14,8 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+    }
+    public Optional<Movie> singleMovie(ObjectId id){
+        return movieRepository.findById(id);
     }
 }
