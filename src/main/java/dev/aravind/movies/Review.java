79d8289b2cf5cd86ef8044df744208193ca714f1
avd <aravind19112003@gmail.com>
2023-11-28 14:32:29 +0530

    Movie Controller get mapping

---
+++ src/main/java/dev/aravind/movies/Review.java
@@ -0,0 +1,18 @@
+package dev.aravind.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+}
