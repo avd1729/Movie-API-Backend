b882d6956ae201112f4d5807c974001a99f84ffe
avd <aravind19112003@gmail.com>
2023-11-28 15:35:03 +0530

    Search by ImdbId

--- src/main/java/dev/aravind/movies/MovieRepository.java
+++ src/main/java/dev/aravind/movies/MovieRepository.java
@@ -3,7 +3,11 @@ package dev.aravind.movies;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 // Data Access Layer for our REST API , talking to the database and getting data
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
 }
