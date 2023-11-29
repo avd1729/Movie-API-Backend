97e7c20cf3c978da3eb7e17a675208f77ede0f12
avd <aravind19112003@gmail.com>
2023-11-29 11:49:58 +0530

    WebConfig - CorsRegistry

---
+++ src/main/java/dev/aravind/movies/WebConfig.java
@@ -0,0 +1,21 @@
+package dev.aravind.movies;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/movies")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+        registry.addMapping("/api/v1/reviews")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
+
